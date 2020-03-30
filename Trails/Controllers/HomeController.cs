aac6e623850a2a0d02fbe6e2145a157c3a8176c0
rakesharjh <47509674+rakesharjh@users.noreply.github.com>
2020-03-30 16:03:07 +0530

    Add project files.

---
+++ Trails/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Trails.Models;
+
+namespace Trails.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+         [HttpGet]
+        public IActionResult Edit()
+        {
+            var test = new Test();
+            return PartialView("_showAjaxWorks", test);
+
+        }
+
+        [HttpGet]
+        public IActionResult ShowEdit()
+        {
+            var test = new Test();
+            return PartialView("_viewAjaxworks",test);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id)
+        {
+            var test = new Test();
+            return PartialView("_viewAjaxworks", test);
+
+        }
+    }
+}
